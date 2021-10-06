1ccff9197daf32e72aab7c16977b857078e134f3
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-10-06 17:00:14 -0600

    rename from_url as html_from_url to avoid conflicts

--- commmons/html.py
+++ commmons/html.py
@@ -3,7 +3,7 @@ from lxml import html
 from lxml.html import HtmlElement
 
 
-def from_url(url, **kwargs) -> HtmlElement:
+def html_from_url(url, **kwargs) -> HtmlElement:
     r = requests.get(url, **kwargs)
     r.raise_for_status()
     return html.fromstring(r.text)
