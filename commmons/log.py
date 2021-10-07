71414d2701a83d4fb14018498533d89a87eb6d84
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-10-06 18:55:56 -0600

    add get_file_handler

--- commmons/log.py
+++ commmons/log.py
@@ -1,7 +1,9 @@
 import logging
+import os
 
 __all__ = [
-    "get_prefixed_logger"
+    "get_prefixed_logger",
+    "get_file_handler"
 ]
 
 
@@ -11,3 +13,16 @@ def get_prefixed_logger(parent_logger, prefix) -> logging.Logger:
             return '[%s] %s' % (self.extra['prefix'], msg), kwargs
 
     return CustomAdapter(parent_logger, {'prefix': prefix})
+
+
+def get_file_handler(path: str,
+                     fmt: str = "%(asctime)s %(levelname)-5s %(funcName)-26s %(message)s",
+                     datefmt: str = "%Y-%m-%d %H:%M:%S") -> logging.FileHandler:
+    assert os.path.exists(path)
+
+    formatter = logging.Formatter(fmt)
+    formatter.datefmt = datefmt
+
+    handler = logging.FileHandler(path)
+    handler.setFormatter(formatter)
+    return handler
