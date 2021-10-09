54dd09d0ff40b6e3ea3f8cce7e7f0fb55b0ed6f1
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-10-08 19:32:40 -0600

    touch directory

--- commmons/fs.py
+++ commmons/fs.py
@@ -15,9 +15,13 @@ def get_filesize_in_bytes(path: str):
     return s.st_size
 
 
-def touch(path: str):
+def touch(path: str, is_directory=False):
     if os.path.exists(path):
         return
 
     os.makedirs(Path(path).parent, exist_ok=True)
     open(path, "a").close()
+
+
+def touch_directory(path: str):
+    touch(path, is_directory=True)
